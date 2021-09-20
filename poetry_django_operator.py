4dae2c62463e7276396d97976635fd81b3668833
meelan-vg <meelan.lakhoo@vatglobal.com>
2021-09-20 14:37:54 +0200

    Added support for args and kwargs

--- poetry_django_operator.py
+++ poetry_django_operator.py
@@ -18,9 +18,9 @@ class PoetryDjangoOperator(BaseOperator):
             project_stage: str = None,
             function_name: str = None,
             function_file_path: str = None,
+            string_args: Optional[Iterable[str]] = None,
             op_args: Optional[List] = None,
             op_kwargs: Optional[Dict] = None,
-            string_args: Optional[Iterable[str]] = None,
             templates_dict: Optional[Dict] = None,
             templates_exts: Optional[List[str]] = None,
             **kwargs,
@@ -47,9 +47,11 @@ class PoetryDjangoOperator(BaseOperator):
 
     def _write_script_file(self, script_filename, script_template_file):
         python_source = (
-            "def execution_function():\n"
+            "def execution_function(*args, **kwargs):\n"
+            f"    print('args', args)\n"
+            f"    print('kwargs', kwargs)\n"
             f"    from {self.function_file_path} import {self.function_name}\n"
-            f"    {self.function_name}()\n"
+            f"    {self.function_name}(*args, **kwargs)\n"
         )
 
         write_python_script(
@@ -71,6 +73,7 @@ class PoetryDjangoOperator(BaseOperator):
                 f'os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vsuite.config.settings") \n'
             )
             script_file.write("BASE_DIR = os.path.dirname(os.path.dirname(__file__)) \n\n")
+            script_file.write("print('Current Base DIR' , os.getenv('BASE_DIR')) \n\n")
             script_file.write("django.setup() \n\n")
             for template_line in open(script_template_file):
                 script_file.write(template_line)
@@ -86,16 +89,14 @@ class PoetryDjangoOperator(BaseOperator):
                 raise
 
     def execute(self, context: Dict):
+        self.op_kwargs['dag_run_conf'] = context['dag_run'].conf
         context.update(self.op_kwargs)
         context["templates_dict"] = self.templates_dict
 
-        # TODO kwargs support
-        op_kwargs = {}
-        self.op_kwargs = op_kwargs
-
         if self.templates_dict:
             self.op_kwargs["templates_dict"] = self.templates_dict
-        random_string = str(datetime.datetime.now().strftime("%d-%m-%Y")) + "--" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        random_string = str(datetime.datetime.now().strftime("%d-%m-%Y")) + "--" + ''.join(
+            random.choices(string.ascii_uppercase + string.digits, k=6))
         tmp_dir = f'/opt/airflow/dags/{self.project_stage}/{self.project_name}'
 
         input_filename = os.path.join(tmp_dir, f"{random_string}-script-airflow-system.in")
